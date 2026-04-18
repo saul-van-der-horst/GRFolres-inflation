0ec656246b7fe0838509a05f5877654d2ddeaa12
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-18 13:00:01 +0100

    Add additional diagnostic variables for analysis

--- Examples/InflationCubicHorndeski/DiagnosticVariables.hpp
+++ Examples/InflationCubicHorndeski/DiagnosticVariables.hpp
@@ -11,14 +11,22 @@ enum
 {
     c_Ham,         // Hamiltonian constraint
     c_Mom,         // Momentum constraints
+    c_Mom2,
+    c_Mom3,
     c_Ham_abs_sum, // Sum of absolute value of each term in Ham
     c_Mom_abs_sum, // Sum of absolute value of each term in Mom
     c_rho,         // Energy density
     c_sqrt_gamma,  // \sqrt(\gamma) = pow(chi,-3/2) volume factor of spatial
                    // metric
+    c_Weyl4_Re,
+    c_Weyl4_Im,
     c_rho_scaled,  // \rho * volume factor
     c_S_scaled,    // S * volume factor
     c_K_scaled,    // K * volume factor
+    c_rho_phi,
+    c_rho_g2,
+    c_rho_g3,
+    c_rho_GB,
 
     NUM_DIAGNOSTIC_VARS
 };
@@ -26,9 +34,13 @@ enum
 namespace DiagnosticVariables
 {
 static const std::array<std::string, NUM_DIAGNOSTIC_VARS> variable_names = {
-    "Ham",
+    "Ham",      "Mom1",     "Mom2",
+    "Mom3",
 
-    "Mom",
+    "Weyl4_Re", "Weyl4_Im",
+
+    "rho_phi",  "rho_g2",   "rho_g3",
+    "rho_GB"
 
     "Ham_abs",    "Mom_abs",  "rho",     "sqrt_gamma",
     "rho_scaled", "S_scaled", "K_scaled"};
