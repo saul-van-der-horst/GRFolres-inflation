61e832aa3d872f7d8748048bfe8e3067ae57723a
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-27 00:36:15 +0100

    Refactor CosmoDiagnostics to use theory_t instead of matter_t

--- Examples/InflationCubicHorndeski/CosmoDiagnostics.hpp
+++ Examples/InflationCubicHorndeski/CosmoDiagnostics.hpp
@@ -6,14 +6,13 @@
 #ifndef COSMODIAGNOSTICS_HPP_
 #define COSMODIAGNOSTICS_HPP_
 
+#include "BSSNVars.hpp"
 #include "CCZ4Geometry.hpp"
-#include "CCZ4Vars.hpp"
 #include "Cell.hpp"
 #include "Coordinates.hpp"
 #include "DiagnosticVariables.hpp"
 #include "FourthOrderDerivatives.hpp"
 #include "GRInterval.hpp"
-#include "NewMatterConstraints.hpp"
 #include "Tensor.hpp"
 #include "simd.hpp"
 #include <array>
@@ -27,13 +26,14 @@ class CosmoDiagnostics // public MatterConstraints<matter_t>
   public:
     // Inherit the variable definitions from CCZ4 + matter_t
     template <class data_t>
-    using CCZ4Vars = typename CCZ4::template Vars<data_t>;
+    using TheoyVars = typename theory_t::template Vars<data_t>;
 
     template <class data_t>
-    using MatterVars = typename matter_t::template Vars<data_t>;
-
+    using TheoryVars = typename theory_t::template Vars<data_t>;
+ 
     template <class data_t>
-    struct Vars : public CCZ4Vars<data_t>, public MatterVars<data_t>
+    struct BSSNTheoryVars : public BSSNVars::VarsNoGauge<data_t>,
+                            public TheoryVars<data_t>
     {
         /// Defines the mapping between members of Vars and Chombo grid
         /// variables (enum in User_Variables)
@@ -46,8 +46,8 @@ class CosmoDiagnostics // public MatterConstraints<matter_t>
     };
 
     //! Constructor of class CosmoDiagnostics
-    CosmoDiagnostics(const matter_t a_matter, double dx, double G_Newton)
-        : m_matter(a_matter), m_deriv(dx)
+    CosmoDiagnostics(const theory_t a_theory, double a_dx,
+                     const std::array<double, CH_SPACEDIM> a_center);
     {
     }
 
@@ -56,7 +56,8 @@ class CosmoDiagnostics // public MatterConstraints<matter_t>
     template <class data_t> void compute(Cell<data_t> current_cell) const;
 
   protected:
-    matter_t m_matter; //!< The matter object, e.g. a scalar field
+    theory_t m_ytheory; //!< The matter object, e.g. a scalar field
+    const std::array<double, CH_SPACEDIM> m_center;
     const FourthOrderDerivatives m_deriv;
 };
 
