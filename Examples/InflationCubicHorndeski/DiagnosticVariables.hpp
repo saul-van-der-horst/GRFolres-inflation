4cb8d72764d527094c22ece15bdbb1c45426b79a
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-26 13:09:50 +0100

    Refactor diagnostic variable enumeration and names

--- Examples/InflationCubicHorndeski/DiagnosticVariables.hpp
+++ Examples/InflationCubicHorndeski/DiagnosticVariables.hpp
@@ -10,16 +10,17 @@
 enum
 {
     c_Ham,         // Hamiltonian constraint
-    c_Mom,         // Momentum constraints
+    c_Mom1,         // Momentum constraints
     c_Mom2,
     c_Mom3,
     c_Ham_abs_sum, // Sum of absolute value of each term in Ham
-    c_Mom_abs_sum, // Sum of absolute value of each term in Mom
-    c_rho,         // Energy density
+    c_Mom_abs_sum1, // Sum of absolute value of each term in Mom
+    c_Mom_abs_sum2,
+    c_Mom_abs_sum3,
+   
     c_sqrt_gamma,  // \sqrt(\gamma) = pow(chi,-3/2) volume factor of spatial
                    // metric
-    c_Weyl4_Re,
-    c_Weyl4_Im,
+    
     c_rho_scaled,  // \rho * volume factor
     c_S_scaled,    // S * volume factor
     c_K_scaled,    // K * volume factor
@@ -36,14 +37,14 @@ namespace DiagnosticVariables
 static const std::array<std::string, NUM_DIAGNOSTIC_VARS> variable_names = {
     "Ham",      "Mom1",     "Mom2",
     "Mom3",
-
-    "Weyl4_Re", "Weyl4_Im",
+    "Ham_abs",    "Mom_abs1","Mom_abs2","Mom_abs3",  "rho",     "sqrt_gamma",
+    "rho_scaled", "S_scaled", "K_scaled",
+    
 
     "rho_phi",  "rho_g2",   "rho_g3",
     "rho_GB"
 
-    "Ham_abs",    "Mom_abs",  "rho",     "sqrt_gamma",
-    "rho_scaled", "S_scaled", "K_scaled"};
+    };
 }
 
 #endif /* DIAGNOSTICVARIABLES_HPP */
