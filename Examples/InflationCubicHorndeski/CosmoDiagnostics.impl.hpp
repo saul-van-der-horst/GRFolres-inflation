3c0c029d31e5243f0ac200495e6ef12d004a8f14
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-04-26 20:49:45 +0100

    Refactor energy momentum tensor calculations

--- Examples/InflationCubicHorndeski/CosmoDiagnostics.impl.hpp
+++ Examples/InflationCubicHorndeski/CosmoDiagnostics.impl.hpp
@@ -34,21 +34,39 @@ void CosmoDiagnostics<matter_t>::compute(Cell<data_t> current_cell) const
     data_t a;
 
     // Energy Momentum Tensor
-    const auto emtensor = m_matter.compute_emtensor(vars, d1, h_UU, chris.ULL);
+    const auto rho_and_Si =
+        theory_t.compute_rho_and_Si(vars, d1, d2, coords);
+    const auto Sij_TF_and_S =
+        theory_t.compute_Sij_TF_and_S(vars, d1, d2,
+                                       /*advec=*/vars, // advec not used here
+                                       coords);
+    const auto all_rhos =
+        theory_t.compute_all_rhos(vars, d1, d2, coords);
 
-    sqrt_gamma = pow(vars.chi, -3. / 2.);
-    rho = emtensor.rho;
-    S = emtensor.S;
-    K_scaled = vars.K / pow(vars.chi, 3. / 2.);
-    rho_scaled = emtensor.rho / pow(vars.chi, 3. / 2.);
-    S_scaled = emtensor.S / pow(vars.chi, 3. / 2.);
-
+    const data_t sqrt_gamma = pow(vars.chi, -3. / 2.);
+    const data_t rho       = rho_and_Si.rho;
+    const data_t S         = Sij_TF_and_S.S;
+    const data_t K_scaled = vars.K / pow(vars.chi, 3. / 2.);
+    const data_t rho_scaled = rho / pow(vars.chi, 3. / 2.);
+    const data_t S_scaled = S / pow(vars.chi, 3. / 2.);
+    const data_t rho_phi   = all_rhos.phi;
+    const data_t rho_g2    = all_rhos.g2;
+    const data_t rho_g3    = all_rhos.g3;
+    const data_t rho_phi_scaled = rho_phi / pow(vars.chi, 3. / 2.);
+    const data_t rho_g2_scaled  = rho_g2 / pow(vars.chi, 3. / 2.);
+    const data_t rho_g3_scaled  = rho_g3 / pow(vars.chi, 3. / 2.);
     // Write the diagnostics into the output vector of cells
     current_cell.store_vars(sqrt_gamma, c_sqrt_gamma);
     current_cell.store_vars(rho, c_rho);
     current_cell.store_vars(rho_scaled, c_rho_scaled);
     current_cell.store_vars(S_scaled, c_S_scaled);
     current_cell.store_vars(K_scaled, c_K_scaled);
+    current_cell.store_vars(rho_phi,       c_rho_phi);
+    current_cell.store_vars(rho_phi_scaled,c_rho_phi_scaled);
+    current_cell.store_vars(rho_g2,        c_rho_g2);
+    current_cell.store_vars(rho_g2_scaled, c_rho_g2_scaled);
+    current_cell.store_vars(rho_g3,        c_rho_g3);
+    current_cell.store_vars(rho_g3_scaled, c_rho_g3_scaled);
 }
 
 #endif /* COSMODIAGNOSTICS_IMPL_HPP_ */
