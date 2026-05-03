7b3b252fc8dbbc8ec97f8bf6d1baab965d2d8d3d
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-05-03 14:54:42 +0100

    Refactor CosmoDiagnostics class constructor and variables

--- Examples/InflationCubicHorndeski/CosmoDiagnostics.hpp
+++ Examples/InflationCubicHorndeski/CosmoDiagnostics.hpp
@@ -2,7 +2,6 @@
  * Copyright 2012 The GRChombo collaboration.
  * Please refer to LICENSE in GRChombo's root directory.
  */
-
 #ifndef COSMODIAGNOSTICS_HPP_
 #define COSMODIAGNOSTICS_HPP_
 
@@ -17,50 +16,51 @@
 #include "simd.hpp"
 #include <array>
 
-//! Calculates all relevant variables, which
-//! are stored as diagnostics
-
 template <class theory_t>
-class CosmoDiagnostics // public MatterConstraints<matter_t>
+class CosmoDiagnostics
 {
   public:
-    // Inherit the variable definitions from CCZ4 + matter_t
+    // Combined BSSN + theory variables loaded at each cell
     template <class data_t>
     using TheoryVars = typename theory_t::template Vars<data_t>;
 
     template <class data_t>
-    using TheoryVars = typename theory_t::template Vars<data_t>;
- 
-    template <class data_t>
-    struct BSSNTheoryVars : public BSSNVars::VarsNoGauge<data_t>,
-                            public TheoryVars<data_t>
+    struct Vars : public BSSNVars::VarsNoGauge<data_t>,
+                  public TheoryVars<data_t>
     {
-        /// Defines the mapping between members of Vars and Chombo grid
-        /// variables (enum in User_Variables)
         template <typename mapping_function_t>
         void enum_mapping(mapping_function_t mapping_function)
         {
-            CCZ4Vars<data_t>::enum_mapping(mapping_function);
+            BSSNVars::VarsNoGauge<data_t>::enum_mapping(mapping_function);
             TheoryVars<data_t>::enum_mapping(mapping_function);
         }
     };
 
-    //! Constructor of class CosmoDiagnostics
-    CosmoDiagnostics(const theory_t a_theory, double a_dx,
-                     const std::array<double, CH_SPACEDIM> a_center);
+    //! Constructor
+    CosmoDiagnostics(const theory_t a_theory,
+                     double a_dx,
+                     double a_G_Newton,
+                     double a_chi_background,
+                     const std::array<double, CH_SPACEDIM> a_center)
+        : m_theory(a_theory),
+          m_deriv(a_dx),
+          m_G_Newton(a_G_Newton),
+          m_chi_background(a_chi_background),
+          m_center(a_center)
     {
     }
 
-    //! The compute member which calculates the diagnostics at each point in the
-    //! box
-    template <class data_t> void compute(Cell<data_t> current_cell) const;
+    //! Compute diagnostics at each cell
+    template <class data_t>
+    void compute(Cell<data_t> current_cell) const;
 
   protected:
-    theory_t m_theory; //!< The matter object, e.g. a scalar field
-    const std::array<double, CH_SPACEDIM> m_center;
+    theory_t m_theory;
     const FourthOrderDerivatives m_deriv;
+    const double m_G_Newton;
+    const double m_chi_background;   // a(t)^{-2}, set from spatial mean each step
+    const std::array<double, CH_SPACEDIM> m_center;
 };
 
 #include "CosmoDiagnostics.impl.hpp"
-
 #endif /* COSMODIAGNOSTICS_HPP_ */
