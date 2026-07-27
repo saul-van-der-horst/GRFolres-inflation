8576ddae44b4284f7b597348e2a8876466ebbb6f
saul-van-der-horst <vanderhorstsaul57@gmail.com>
2026-07-27 15:24:19 +0100

    Replace CouplingAndPotential with DefaultCouplingAndPotential

--- Source/CubicHorndeski/CubicHorndeski.hpp
+++ Source/CubicHorndeski/CubicHorndeski.hpp
@@ -8,7 +8,7 @@
 
 #include "CCZ4Geometry.hpp"
 #include "Coordinates.hpp"
-#include "CouplingAndPotential.hpp"
+#include "DefaultCouplingAndPotential.hpp"
 #include "FourthOrderDerivatives.hpp"
 #include "ModifiedCCZ4RHS.hpp"
 #include "Tensor.hpp"
@@ -37,7 +37,7 @@
      \sa ModifiedCCZ4RHS(), ModifiedGravityConstraints()
 */
 
-template <class coupling_and_potential_t = CouplingAndPotentialAdapter>
+template <class coupling_and_potential_t = DefaultCouplingAndPotential>
 class CubicHorndeski
 {
   public:
@@ -60,11 +60,11 @@ class CubicHorndeski
         Tensor<1, data_t> tau_ij_dot_dphi;
         data_t tau_ij_dot_dphi2;
     };
-     CubicHorndeski(const coupling_and_potential_t a_coupling_and_potential)
-    : my_coupling_and_potential(a_coupling_and_potential)
-     {
-     }
-    
+
+    CubicHorndeski(const coupling_and_potential_t a_coupling_and_potential)
+        : my_coupling_and_potential(a_coupling_and_potential)
+    {
+    }
 
     template <class data_t> struct Vars
     {
@@ -169,7 +169,7 @@ class CubicHorndeski
         const; //!< the value of the coordinates
 
   protected:
-    const coupling_and_potential_t my_coupling_and_potential;
+    coupling_and_potential_t my_coupling_and_potential;
 };
 
 #include "CubicHorndeski.impl.hpp"
